b3b8525909fd4c8d3a3c6ec51caedc752330b252
onifadjosh <onifadjosh@gmail.com>
2026-02-24 10:15:02 +0100

    Added cors middleware and cloudinary dependency

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const ejs = require('ejs')
 const mongoose = require("mongoose")
+const cors = require("cors")
 app.set("view engine", 'ejs')
 const dotenv = require("dotenv");
 dotenv.config();
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(cors())
 const UserRouter = require('./routers/user.routes')
 app.use('/api/v1', UserRouter)
 
